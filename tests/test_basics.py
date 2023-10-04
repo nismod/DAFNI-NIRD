bdffe9c3ab0f31262c8e789ebfd5ca1e71e3ab07
Tom Russell <tom.russell@ouce.ox.ac.uk>
2023-10-04 15:14:25 +0100

    Add test file

---
+++ tests/test_basics.py
@@ -0,0 +1,11 @@
+"""Initial test file
+"""
+from nird import add_one
+
+
+def test_add_one() -> None:
+    assert add_one(3) == 4
+
+
+def test_add_one_float() -> None:
+    assert add_one(3.5) == 4.5
