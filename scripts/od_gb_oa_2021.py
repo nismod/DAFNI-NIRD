140248fc1ccd8c7cbf38505f5202205c9f135b6f
YUE LI <yue.li@ouce.ox.ac.uk>
2024-08-12 17:25:33 +0100

    Prepare for OD data (admin level vs network level)

---
+++ scripts/od_gb_oa_2021.py
@@ -0,0 +1,234 @@
+""" OD data preparation
+- Disaggregate OD matrix from multi-admin-layers to OA level
+- Predict OD matrix from 2011 to 2021
+
+Input OD matrix: Number of travel-to-work trips, UK, 2011, Multi-admins
+Output OD matrix: Numnber of travel-to-work trips, GB, 2021, OA level.
+"""
+
+from pathlib import Path
+import os
+import pandas as pd
+from collections import defaultdict
+from nird.utils import load_config
+import random
+import warnings
+
+warnings.simplefilter("ignore")
+
+base_path = Path(load_config()["paths"]["base_path"])
+base_path = base_path / "census_datasets" / "od_matrix"
+
+# %%
+# Scottish population (2011) at LSOA and OA levels
+pop11_Scot = pd.read_csv(os.path.join(base_path, "pop11_scot.csv"))
+pop11_Scot_lsoa = pop11_Scot.groupby("LSOA11CD", as_index=False).agg({"Popcount": sum})
+pop11_Scot_oa = pop11_Scot[["OA11CD", "Popcount"]]
+
+# Scottish population (2021) at LSOA level
+pop21_Scot_lsoa = pd.read_excel(
+    os.path.join(base_path, "sape-2021.xlsx"), sheet_name="Sheet1"
+)
+pop21_Scot_lsoa.rename(
+    columns={"Data zone code": "LSOA21CD", "Total population": "Popcount"}, inplace=True
+)
+
+# (Population disaggregation) to estimate Scottish population (2021) at OA level
+temp_pop = pd.merge(pop11_Scot, pop11_Scot_lsoa, on="LSOA11CD", how="left")
+temp_pop["oa/lsoa"] = temp_pop.Popcount_x / temp_pop.Popcount_y
+dict_ratio = defaultdict(lambda: defaultdict(list))
+for idx, row in temp_pop.iterrows():
+    oa = row["OA11CD"]
+    lsoa = row["LSOA11CD"]
+    rt = row["oa/lsoa"]
+    dict_ratio[lsoa][oa] = rt
+
+oaList = []
+popList = []
+for idx, row in pop21_Scot_lsoa.iterrows():
+    lsoa = row["LSOA21CD"]
+    pop = row["Popcount"]
+    if lsoa in dict_ratio.keys():
+        for oa, ratio in dict_ratio[lsoa].items():
+            oaList.append(oa)
+            popList.append(pop * ratio)
+
+pop21_Scot_oa = pd.DataFrame({"OA21CD": oaList, "Popcount": popList})
+
+# %%
+# Read OD data at multiple admin levels
+od11_gb = pd.read_csv(
+    os.path.join(base_path, "od_gb_2011_multiAdminLevel.csv")
+)  # source: WU03BUK_oa_wz_v4 (2011)
+
+# Extract od origining from Scotland (2011)
+od11_Scot_oa = od11_gb[od11_gb.from_region == "oa_scot"]
+od11_Scot_oa.reset_index(drop=True, inplace=True)
+od11_Scot_oa.rename(columns={"car": "travel11"}, inplace=True)
+
+# Project OD from 2011 to 2021: travel21
+pop11_scot_dict = pop11_Scot_oa.set_index("OA11CD")["Popcount"].to_dict()
+pop21_scot_dict = pop21_Scot_oa.set_index("OA21CD")["Popcount"].to_dict()
+od11_Scot_oa["Pop11"] = od11_Scot_oa["Area of usual residence"].map(pop11_scot_dict)
+od11_Scot_oa["Pop21"] = od11_Scot_oa["Area of usual residence"].map(pop21_scot_dict)
+od11_Scot_oa["travel21"] = (
+    od11_Scot_oa.travel11 * od11_Scot_oa.Pop21 / od11_Scot_oa.Pop11
+)
+od11_Scot_oa["travel21"] = od11_Scot_oa["travel21"].round()  # round the number of trips
+# -> pop11, pop21, travel11, travel21
+
+# %%
+# OD disaggregation
+od11_Scot_oa = od11_Scot_oa[
+    od11_Scot_oa["Area of usual residence"] != od11_Scot_oa["Area of workplace"]
+]
+od11_Scot_oa.reset_index(drop=True, inplace=True)
+
+# Trip Classification
+# within Scotland (oa-oa) -> keep
+od11_scotoa_scotoa = od11_Scot_oa[od11_Scot_oa.to_region != "msoa_enw"]
+od11_scotoa_scotoa.reset_index(drop=True, inplace=True)
+
+# %%
+# Extract OD from Scotland to ENW (oa-msoa) -> disaggregate
+od11_scotoa_enwmsoa = od11_Scot_oa[od11_Scot_oa.to_region == "msoa_enw"]
+od11_scotoa_enwmsoa.reset_index(drop=True, inplace=True)
+
+# ENW 2011/21 population at OA level
+pop11_enw_oa = pd.read_csv(os.path.join(base_path, "pop11_enw_oa.csv"))
+pop11_enw_oa_dict = pop11_enw_oa.set_index("OA11CD")["POP11"].to_dict()
+pop21_enw_oa = pd.read_csv(os.path.join(base_path, "gb_population_2021_estimates.csv"))
+pop21_enw_oa_dict = pop21_enw_oa.set_index("OA21CD")["OA_POP_2021"].to_dict()
+
+# from MSOA11 to OA11: MSOA11 contains multiple OA11
+lut_enw_msoa11_oa11 = pd.read_csv(
+    os.path.join(base_path, "lut\\_lut_enw_msoa11_oa11.csv")
+)
+msoa11_to_oa11_dict = (
+    lut_enw_msoa11_oa11.groupby(by=["MSOA11CD"], as_index=False)
+    .agg({"OA11CD": list})
+    .set_index("MSOA11CD")["OA11CD"]
+    .to_dict()
+)
+msoa11_oa11_pop11 = defaultdict(lambda: defaultdict(list))
+for msoa11 in msoa11_to_oa11_dict.keys():
+    for oa11 in msoa11_to_oa11_dict[msoa11]:
+        pop11 = pop11_enw_oa_dict[oa11]
+        msoa11_oa11_pop11[msoa11][oa11] = pop11  # 2011 OA population
+
+# from OA11 to OA21: OA11 may contain multiple OA21
+lut_enw_oa11_oa21 = pd.read_csv(os.path.join(base_path, "lut\\_lut_enw_oa11_oa21.csv"))
+oa11_to_oa21_dict = (
+    lut_enw_oa11_oa21.groupby(by=["OA11CD"]).agg({"OA21CD": list}).to_dict()["OA21CD"]
+)
+oa11_oa21_pop21 = defaultdict(lambda: defaultdict(list))
+for oa11 in oa11_to_oa21_dict.keys():
+    for oa21 in oa11_to_oa21_dict[oa11]:
+        pop21 = pop21_enw_oa_dict[oa21]
+        oa11_oa21_pop21[oa11][oa21] = pop21  # 2021 OA population
+
+# %%
+# Estimate msoa11_oa21_pop21
+msoa11_oa21_pop21 = defaultdict(lambda: defaultdict(list))
+for msoa11, list_of_oa11 in msoa11_oa11_pop11.items():
+    for oa11 in list_of_oa11.keys():
+        if oa11 in oa11_oa21_pop21.keys():
+            for oa21, pop21 in oa11_oa21_pop21[oa11].items():
+                msoa11_oa21_pop21[msoa11][oa21] = pop21
+        else:
+            print(f"We cannot find oa21 for {oa11}")
+
+msoa11_oa21_ratio21 = defaultdict(lambda: defaultdict(list))
+for msoa11, list_of_oa21 in msoa11_oa21_pop21.items():
+    ttpop = sum(msoa11_oa21_pop21[msoa11].values())
+    for oa21, pop21 in msoa11_oa21_pop21[msoa11].items():
+        percentage = pop21 / ttpop
+        msoa11_oa21_ratio21[msoa11][oa21] = percentage
+
+msoa11_oa21_ratio21_norm = defaultdict(lambda: defaultdict(list))
+for msoa11, list_of_oa21 in msoa11_oa21_ratio21.items():
+    sum_ratio = sum(msoa11_oa21_ratio21[msoa11].values())
+    for oa21, ratio in list_of_oa21.items():
+        normalised_ratio = ratio / sum_ratio
+        msoa11_oa21_ratio21_norm[msoa11][oa21] = normalised_ratio
+
+# Disaggregate OD 2021 to OA level
+assignment = {}  # scot[oa21] = travel21
+for idx, row in od11_scotoa_enwmsoa.iterrows():  # each: 0 ~ 16; total: 8631
+    scot11 = row["Area of usual residence"]
+    msoa11 = row["Area of workplace"]
+    list_of_oa21 = list(msoa11_oa21_ratio21_norm[msoa11].keys())
+    # initialisation
+    if scot11 not in assignment:
+        assignment[scot11] = {oa21: 0 for oa21 in list_of_oa21}
+    else:
+        for oa21 in list_of_oa21:
+            if oa21 not in assignment[scot11]:
+                assignment[scot11][oa21] = 0
+    # update
+    travel21 = row["travel21"]
+    list_of_prob21 = list(msoa11_oa21_ratio21_norm[msoa11].values())
+    for _ in range(int(travel21)):
+        selected_oa21 = random.choices(list_of_oa21, list_of_prob21)[0]
+        assignment[scot11][selected_oa21] += 1
+
+scot11List = []
+oa21List = []
+travel21List = []
+for scot11, v in assignment.items():
+    for oa21, travel21 in v.items():
+        if travel21 > 0:
+            scot11List.append(scot11)
+            oa21List.append(oa21)
+            travel21List.append(travel21)
+
+temp_df = pd.DataFrame(
+    {
+        "Area of usual residence": scot11List,
+        "Area of workplace": oa21List,
+        "travel21": travel21List,
+    }
+)
+
+temp_df = temp_df.groupby(
+    by=["Area of usual residence", "Area of workplace"],
+    as_index=False,
+).agg({"travel21": sum})
+
+# %%
+# combine dataframes
+# Scotland (from OA to OA)
+temp_od11_scotoa_scotoa = od11_scotoa_scotoa[
+    ["Area of usual residence", "Area of workplace", "travel21"]
+]
+od21_scot_oa = pd.concat([temp_df, temp_od11_scotoa_scotoa], axis=0, ignore_index=True)
+od21_scot_oa.rename(columns={"travel21": "Car21"}, inplace=True)
+
+# %%
+# England and Wales (from OA to OA: real observation data)
+od21_enw_oa = pd.read_csv(os.path.join(base_path, "od_enw_2021\\ODWP01EW_OA.csv"))
+od21_enw_oa.rename(
+    columns={
+        "Output Areas code": "Area of usual residence",
+        "OA of workplace code": "Area of workplace",
+        "Count": "Car21",
+    },
+    inplace=True,
+)
+od21_enw_oa = od21_enw_oa[
+    (od21_enw_oa["Place of work indicator (4 categories) code"] == 3)
+    & (od21_enw_oa["Area of usual residence"] != od21_enw_oa["Area of workplace"])
+]
+od21_enw_oa = od21_enw_oa[["Area of usual residence", "Area of workplace", "Car21"]]
+
+# remove North Irland
+od21_enw_oa = od21_enw_oa[~od21_enw_oa["Area of workplace"].str.startswith("N")]
+od21_enw_oa.reset_index(drop=True, inplace=True)
+
+# %%
+# Combined OD 2021 at OA level
+od21_gb_oa = pd.concat([od21_scot_oa, od21_enw_oa], axis=0, ignore_index=True)
+
+# %%
+# export file
+od21_gb_oa.to_csv(base_path / "od_gb_oa_2021.csv", index=False)
---
+++ scripts/od_gb_oa_2021_node.py
@@ -0,0 +1,265 @@
+"""Assign the OD inbound/outbound trips to road nodes
+- using GB residential buildings to approximate the concentration of usual residences
+- using GB non-residential buildings to approximate the concentration of workplaces
+- if buildings info is not available, using OA centroids instead
+"""
+
+from pathlib import Path
+import geopandas as gpd
+import pandas as pd
+import random
+from nird.utils import load_config
+import nird.road as func
+from collections import defaultdict
+from tqdm.auto import tqdm
+import pickle
+import warnings
+
+warnings.simplefilter("ignore")
+
+incoming_path = Path(load_config()["paths"]["incoming_data"])
+base_path = Path(load_config()["paths"]["base_path"])
+
+# %%
+# UK buildings
+verisk = gpd.read_file(
+    incoming_path / "census" / "VERISK_gb" / "data" / "edition_15_online_version.gpkg",
+    engine="pyogrio",
+)  # geometry: polygon (5mins)
+
+# building reclassification rules (-> residential/non-residential)
+rule_file = pd.read_csv(
+    incoming_path / "census" / "VERISK_gb" / "reclassification_rules.csv",
+)
+class_dict = rule_file.set_index("use")["class"].to_dict()
+uniheight_dict = rule_file.set_index("use")["unit_height_m"].to_dict()
+
+# Roads
+road_node_file = gpd.read_parquet(
+    base_path / "networks" / "road" / "road_node_file.geoparquet"
+)  # nd_id
+
+# OA admins (2021)
+admin_file = gpd.read_parquet(
+    base_path
+    / "census_datasets"
+    / "admin_census_boundary_stats"
+    / "gb_oa_2021_estimates.geoparquet",
+)  # OA21CD
+
+# OD Matrix at OA level (2021)
+od_file = pd.read_csv(base_path / "census_datasets" / "od_matrix" / "od_gb_oa_2021.csv")
+od_file.rename(columns={"Car21": "Count21"}, inplace=True)  # 11,143,891 records
+od_file.reset_index(drop=True, inplace=True)
+
+# %%
+# Extract the centroids of UK buildings
+verisk["centroid"] = verisk.geometry.centroid
+verisk_centroids_gdf = verisk.drop(columns=["geometry"]).rename(
+    columns={"centroid": "geometry"}
+)
+verisk_centroids_gdf = gpd.GeoDataFrame(
+    verisk_centroids_gdf, geometry=verisk_centroids_gdf.geometry
+)
+temp = gpd.overlay(
+    verisk_centroids_gdf, admin_file
+)  # intersection (points, polygons), ~25mins
+
+# Extract the centroids of GB buildings
+verisk_gb = verisk[
+    verisk["unique_property_number"].isin(temp["unique_property_number"])
+]
+verisk_gb.reset_index(drop=True, inplace=True)  # unique_property_number
+
+# Attach OA21CD to GB buildings
+dict_build_to_oa = temp.set_index("unique_property_number")["OA21CD"].to_dict()
+verisk_gb["OA21CD"] = verisk_gb["unique_property_number"].map(dict_build_to_oa)
+
+# %%
+# Reclassify buildings into Residential (R) vs Non-residential (NR)
+verisk_gb.loc[(verisk_gb["height"] == 0) | (verisk_gb["height"].isnull()), "height"] = 1
+verisk_gb["class"] = verisk_gb.use.map(class_dict)
+mixeduse = verisk_gb[verisk_gb["class"] == "M"]
+
+# ~25 million residential buildings
+temp = verisk_gb[verisk_gb["class"] == "R"]
+residential = pd.concat([temp, mixeduse], axis=0, ignore_index=True)  # 24,948,681
+residential["unit_height"] = residential.use.map(uniheight_dict)
+residential["num_of_floor"] = residential["height"] / residential["unit_height"]
+residential["gross_area_m2"] = (
+    residential["property_area"] * residential["num_of_floor"]
+)
+residential["gross_area_m2"] = residential["gross_area_m2"].astype(int)  # m2
+
+# ~2 million non-residential buildings
+temp = verisk_gb[verisk_gb["class"] == "N"]
+nonresidential = pd.concat([temp, mixeduse], axis=0, ignore_index=True)  # 1,903,032
+nonresidential["unit_height"] = nonresidential.use.map(uniheight_dict)
+nonresidential["num_of_floor"] = (
+    nonresidential["height"] / nonresidential["unit_height"]
+)
+nonresidential["gross_area_m2"] = (
+    nonresidential["property_area"] * nonresidential["num_of_floor"]
+)
+nonresidential["gross_area_m2"] = nonresidential["gross_area_m2"].astype(int)  # m2
+
+# %%
+# Estimate the total non-residential area (m2) of each OA
+nearest_node_dict = func.find_least_cost_path(nonresidential, road_node_file)
+weight_nr = defaultdict(lambda: defaultdict(list))  # 4 mins: weight[node][oa] = area
+for zidx in range(nonresidential.shape[0]):
+    admin = nonresidential.loc[zidx, "OA21CD"]
+    area = nonresidential.loc[zidx, "gross_area_m2"]
+    nidx = nearest_node_dict[zidx]
+    n = road_node_file.loc[nidx, "nd_id"]
+    weight_nr[n][admin].append(area)
+
+weight_nr2 = defaultdict(lambda: defaultdict(list))
+for node, v1 in weight_nr.items():
+    for oa, list_of_area in v1.items():
+        total_area = sum(list_of_area)
+        weight_nr2[node][oa] = total_area  # format
+weight_nr3 = func.convert_to_dict(weight_nr2)
+
+with open(
+    base_path / "census_datasets" / "verisk" / "weight_non_residential.pkl",
+    "wb",
+) as f:
+    pickle.dump(weight_nr3, f)
+
+# %%
+# Estimate the total residential area (m2) of each OA
+nearest_node_dict = func.find_least_cost_path(residential, road_node_file)
+weight_r = defaultdict(lambda: defaultdict(list))  # 4 mins: weight[node][oa] = area
+for zidx in range(residential.shape[0]):
+    admin = residential.loc[zidx, "OA21CD"]
+    area = residential.loc[zidx, "gross_area_m2"]
+    nidx = nearest_node_dict[zidx]
+    n = road_node_file.loc[nidx, "nd_id"]
+    weight_r[n][admin].append(area)
+
+weight_r2 = defaultdict(lambda: defaultdict(list))
+for node, v1 in weight_r.items():
+    for oa, list_of_area in v1.items():
+        total_area = sum(list_of_area)
+        weight_r2[node][oa] = total_area
+weight_r3 = func.convert_to_dict(weight_r2)
+
+with open(
+    base_path / "census_datasets" / "verisk" / "weight_residential.pkl",
+    "wb",
+) as f:
+    pickle.dump(weight_r3, f)
+
+# %%
+# Within each OA, calculate the probability of each connected road node being considered
+# as a potential OD destination node.
+transformed_weight_nr = {}
+for node, oas in weight_nr3.items():
+    for oa, v in oas.items():
+        if oa not in transformed_weight_nr:
+            transformed_weight_nr[oa] = {}
+        transformed_weight_nr[oa][node] = v
+
+node_weight_nr = defaultdict(lambda: defaultdict(list))
+for oa, list_of_nodes in transformed_weight_nr.items():
+    ttArea = sum(list_of_nodes.values())
+    for node, area in list_of_nodes.items():
+        p = area / ttArea
+        node_weight_nr[oa][node] = round(p, 2)
+
+node_weight_nr = func.convert_to_dict(node_weight_nr)
+with open(
+    base_path / "census_datasets" / "verisk" / "node_weight_non_residential.pkl",
+    "wb",
+) as f:
+    pickle.dump(node_weight_nr, f)
+
+# %%
+# Within each OA, calculate the probability of each connected road node being considered
+# as a potential OD origin node.
+transformed_weight_r = {}
+for node, oas in weight_r3.items():
+    for oa, v in oas.items():
+        if oa not in transformed_weight_r:
+            transformed_weight_r[oa] = {}
+        transformed_weight_r[oa][node] = v
+
+node_weight_r = defaultdict(lambda: defaultdict(list))
+for oa, list_of_nodes in transformed_weight_r.items():
+    ttArea = sum(list_of_nodes.values())
+    for node, area in list_of_nodes.items():
+        p = area / ttArea
+        node_weight_r[oa][node] = round(p, 2)
+
+node_weight_r = func.convert_to_dict(node_weight_r)
+with open(
+    base_path / "census_datasets" / "verisk" / "node_weight_residential.pkl",
+    "wb",
+) as f:
+    pickle.dump(node_weight_r, f)
+
+# %%
+"""
+For OAs without residential buildings, trip origins were approximated using the OA centroids.
+Similarly, for OAs without nonresidential buildings, trip destinations were approximated using the OA centroids.
+"""
+# find the nearest node for each OA centroid (build the reference dict)
+nearest_node_dict = func.find_least_cost_path(admin_file, road_node_file)
+zone_to_node = {}
+for zidx in range(admin_file.shape[0]):
+    z = admin_file.loc[zidx, "OA21CD"]
+    nidx = nearest_node_dict[zidx]
+    n = road_node_file.loc[nidx, "nd_id"]
+    zone_to_node[z] = n
+
+# assign OA-level OD to nodes proportionally
+origins = []  # 15,818,457
+destinations = []
+counts = []
+# invalid_idx = []  # 331,015 invalid OA (3%)
+for idx, row in tqdm(od_file.iterrows(), desc="Processing:", total=od_file.shape[0]):
+    c = row["Count21"]
+    oa_house = row["Area of usual residence"]
+    oa_workplace = row["Area of workplace"]
+    if oa_house in node_weight_r.keys():  # house
+        list_of_origin_nodes = list(node_weight_r[oa_house].keys())
+        list_of_origin_probs = list(node_weight_r[oa_house].values())
+    else:  # if origin does not contain residential building
+        # find the nearest node?
+        # invalid_idx.append(idx)
+        # continue
+        list_of_origin_nodes = [zone_to_node[oa_house]]
+        list_of_origin_probs = [1.0]
+    if oa_workplace in node_weight_nr.keys():  # workplace
+        list_of_destination_nodes = list(node_weight_nr[oa_workplace].keys())
+        list_of_destination_probs = list(node_weight_nr[oa_workplace].values())
+    else:  # if destination does not contain non-residential building
+        # find the nearest node?
+        # invalid_idx.append(idx)
+        # continue
+        list_of_destination_nodes = [zone_to_node[oa_workplace]]
+        list_of_destination_probs = [1.0]
+
+    for _ in range(int(c)):
+        selected_origin_node = random.choices(
+            list_of_origin_nodes, list_of_origin_probs
+        )[0]
+        selected_destination_node = random.choices(
+            list_of_destination_nodes, list_of_destination_probs
+        )[0]
+        origins.append(selected_origin_node)
+        destinations.append(selected_destination_node)
+        counts.append(1)
+
+temp_df = pd.DataFrame(
+    {"origin_node": origins, "destination_node": destinations, "count": counts}
+)
+temp_df_group = temp_df.groupby(
+    by=["origin_node", "destination_node"], as_index=False
+).agg({"count": sum})
+
+# %%
+temp_df_group.to_csv(
+    base_path / "census_datasets" / "od_matrix" / "od_gb_oa_2021_node.csv", index=False
+)
