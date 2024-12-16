80befd1f1ba950920a8dec068196ceecc86f5fd4
YUE LI <yue.li@ouce.ox.ac.uk>
2024-12-16 16:10:17 +0800

    revise road recovery and rerouting module

--- scripts/4_rerouting_and_recovery.py
+++ scripts/4_rerouting_and_recovery.py
@@ -1,3 +1,4 @@
+# %%
 from pathlib import Path
 from functools import partial
 
@@ -10,44 +11,153 @@ import nird.road_revised as func
 
 import json
 import warnings
+from tqdm import tqdm
 
 warnings.simplefilter("ignore")
 
 base_path = Path(load_config()["paths"]["base_path"])
+tqdm.pandas()
 
 
+# %%
 def have_common_items(list1, list2):
     return bool(set(list1) & set(list2))
 
 
+def bridge_recovery(
+    day,
+    damage_level,
+    designed_capacity,
+    current_speed,
+    initial_speed,
+    max_speed,
+    bridge_recovery_dict,
+    acc_speed,
+    acc_capacity,
+):
+    if day == 0:  # occurrence of damage
+        acc_speed = min(current_speed, max_speed)
+        if damage_level == ["no", "minor", "moderate"]:
+            acc_capacity = designed_capacity
+        else:  # extensive/severe
+            acc_capacity = 0.0
+    elif day == 1:  # the first day of recovery
+        acc_speed = initial_speed
+        if damage_level != "no":
+            recover_rate = bridge_recovery_dict[damage_level][day]
+            if recover_rate > 0:
+                acc_capacity = max(designed_capacity * recover_rate, acc_capacity)
+    else:
+        if damage_level != "no":
+            recover_rate = bridge_recovery_dict[damage_level][day]
+            if recover_rate > 0:
+                acc_capacity = max(designed_capacity * recover_rate, acc_capacity)
+
+    return acc_capacity, acc_speed
+
+
+def ordinary_road_recovery(
+    day,
+    damage_level,
+    designed_capacity,
+    current_speed,
+    initial_speed,
+    max_speed,
+    road_recovery_dict,
+    acc_speed,
+    acc_capacity,
+):
+    if day == 0:
+        acc_speed = min(current_speed, max_speed)
+        if damage_level in ["no", "minor", "moderate"]:
+            acc_capacity = designed_capacity
+        else:  # extensive/severe
+            acc_capacity = 0.0
+    elif day == 1:
+        acc_speed = initial_speed
+        if damage_level != "no":
+            recovery_rate = road_recovery_dict[damage_level][day]
+            if recovery_rate > 0:
+                acc_capacity = max(designed_capacity * recovery_rate, acc_capacity)
+    elif day == 2:
+        if damage_level != "no":
+            recovery_rate = road_recovery_dict[damage_level][day]
+            if recovery_rate > 0:
+                acc_capacity = max(designed_capacity * recovery_rate, acc_capacity)
+    else:
+        pass
+
+    return acc_capacity, acc_speed
+
+
 def main():
+    # bridge recovery rates
     with open(base_path / "parameters" / "capt_minor.json", "r") as f:
-        capacity_per_day_minor = json.load(f)
+        bridge_minor = json.load(f)
     with open(base_path / "parameters" / "capt_moderate.json", "r") as f:
-        capacity_per_day_moderate = json.load(f)
+        bridge_moderate = json.load(f)
     with open(base_path / "parameters" / "capt_extensive.json", "r") as f:
-        capacity_per_day_extensive = json.load(f)
+        bridge_extensive = json.load(f)
     with open(base_path / "parameters" / "capt_severe.json", "r") as f:
-        capacity_per_day_severe = json.load(f)
+        bridge_severe = json.load(f)
+
+    # on D-0
+    bridge_minor.insert(0, 0.0)
+    bridge_moderate.insert(0, 0.0)
+    bridge_extensive.insert(0, 0.0)
+    bridge_severe.insert(0, 0.0)
+
+    bridge_recovery_dict = {
+        "minor": bridge_minor,
+        "moderate": bridge_moderate,
+        "extensive": bridge_extensive,
+        "severe": bridge_severe,
+    }
+
+    # road (other than bridge) recovery rates
+    road_minor = [0.0, 1.0, 1.0]
+    road_moderate = [0.0, 1.0, 1.0]
+    road_extensive = [0.0, 1.0, 1.0]
+    road_severe = [0.0, 0.5, 1.0]
+
+    road_recovery_dict = {
+        "minor": road_minor,
+        "moderate": road_moderate,
+        "extensive": road_extensive,
+        "severe": road_severe,
+    }
+
+    # network parameters
     with open(base_path / "parameters" / "flow_cap_plph_dict.json", "r") as f:
         flow_capacity_dict = json.load(f)
     with open(base_path / "parameters" / "flow_breakpoint_dict.json", "r") as f:
         flow_breakpoint_dict = json.load(f)
 
-    assert (
-        len(capacity_per_day_minor)
-        == len(capacity_per_day_moderate)
-        == len(capacity_per_day_extensive)
-        == len(capacity_per_day_severe)
-    ), "inconsistent recovery rates!"
-
-    # road links (with 8 add info)
+    # road links (with 8 added columns)
     road_links = gpd.read_parquet(
-        base_path / "disruption_analysis_1129" / "road_links.pq"
+        base_path / "disruption_analysis_1129" / "road_links2.pq"
     )
+    road_links = func.edge_reclassification_func(road_links)
+    road_links["designed_capacity"] = (
+        road_links.combined_label.map(flow_capacity_dict) * road_links.lanes * 24
+    )
+
+    """How to define disrupted links?
+    - with extensive/severe damage levels
+    - max_speed < current_speed
+    """
     disrupted_links = (
-        road_links.loc[road_links.damage_level != "no", "id"].unique().tolist()
+        road_links.loc[
+            (road_links.max_speed < road_links.current_speed)
+            | (road_links.damage_level == "extensive")
+            | (road_links.damage_level == "severe"),
+            "id",
+        ]
+        .unique()
+        .tolist()
     )
+
+    # extract the disrupted od matrix
     partial_have_common_items = partial(have_common_items, list2=disrupted_links)
     od_path_file = pd.read_parquet(
         base_path.parent / "outputs" / "odpfc_partial_1128.pq"
@@ -57,10 +167,10 @@ def main():
     )
     disrupted_od = od_path_file[od_path_file["disrupted"]].reset_index(drop=True)
     disrupted_od.rename(columns={"flow": "Car21"}, inplace=True)
-    disrupted_od = disrupted_od.head(10)  # for debug
+    disrupted_od = disrupted_od.head(10)  # debug: 76
 
     """
-    recovery process design (-> for mapping):
+    recovery of road capacities (-> for mapping):
                 D-0  D-15  D-30  D-60  D-90  D-110
     minor        0    1     1     1     1     1
     moderate     0    0     0.4   1     1     1
@@ -69,99 +179,71 @@ def main():
     """
 
     # key variables for rerouting analysis:
-    # acc_flow, acc_capacity, acc_speed
+    road_links["disrupted"] = 0
+    road_links.loc[road_links.id.isin(disrupted_links), "disrupted"] = 1
     road_links["acc_flow"] = 0
-    road_links = func.edge_reclassification_func(road_links)
-    road_links["designed_capacity"] = (
-        road_links.combined_label.map(flow_capacity_dict) * road_links.lanes * 24
-    )
-    for day in range(len(capacity_per_day_minor) + 1):  # (0 - 110) - 111 days
-        if day == 0:  # D-0 (occurrence of damage)
-            """edge capacities:
-            - no/minor/moderate: remaining capacities
-            - extensive/severe: closed (zero capacity)
-            """
-            road_links["acc_capacity"] = np.where(
-                (road_links["damage_level"] == "extensive")
-                | (road_links["damage_level"] == "severe"),
-                0,
-                road_links["current_capacity"],
-            )
+    road_links["acc_capacity"] = road_links["current_capacity"]
+    road_links["acc_speed"] = road_links["current_speed"]
 
-            """traffic speeds:
-            - max_speed, current_speed
-            - no/minor/moderate: min(max_speed, current_speed)
-            - extensive/severe: closed (zero speed)
-            """
-            road_links["acc_speed"] = np.where(
-                (road_links["damage_level"] == "extensive")
-                | (road_links["damage_level"] == "severe"),
-                0,
-                np.minimum(road_links["current_speed"], road_links["max_speed"]),
+    for day in range(111):  # Day 0-110
+        (
+            road_links["acc_capacity"],
+            road_links["acc_speed"],
+        ) = zip(
+            *road_links.progress_apply(
+                lambda row: (
+                    bridge_recovery(
+                        day,
+                        row["damage_level"],
+                        row["designed_capacity"],
+                        row["current_speed"],
+                        row["initial_flow_speeds"],
+                        row["max_speed"],
+                        bridge_recovery_dict,
+                        row["acc_speed"],
+                        row["acc_capacity"],
+                    )
+                    if row["disrupted"] == 1 and row["aveBridgeWidth"] > 0
+                    else (
+                        ordinary_road_recovery(
+                            day,
+                            row["damage_level"],
+                            row["designed_capacity"],
+                            row["current_speed"],
+                            row["initial_flow_speeds"],
+                            row["max_speed"],
+                            road_recovery_dict,
+                            row["acc_speed"],
+                            row["acc_capacity"],
+                        )
+                        if row["disrupted"] == 1 and row["aveBridgeWidth"] == 0
+                        else (row["acc_capacity"], row["acc_speed"])
+                    )
+                ),
+                axis=1,
             )
-        else:  # including idle time & traffic reinstatement time
-            """edge capacities: max(acc_capacity, recovered_capacity)"""
-            cap_minor = capacity_per_day_minor[day - 1]
-            cap_moderate = capacity_per_day_moderate[day - 1]
-            cap_extensive = capacity_per_day_extensive[day - 1]
-            cap_severe = capacity_per_day_severe[day - 1]
+        )
 
-            road_links.loc[road_links["damage_level"] == "no", "acc_capacity"] = (
-                road_links["designed_capacity"]
-            )
-            road_links.loc[road_links["damage_level"] == "minor", "acc_capacity"] = (
-                np.maximum(
-                    road_links.loc[
-                        road_links["damage_level"] == "minor", "designed_capacity"
-                    ]
-                    * cap_minor,
-                    road_links.loc[
-                        road_links["damage_level"] == "minor", "acc_capacity"
-                    ],
-                )
-            )
-            road_links.loc[road_links["damage_level"] == "moderate", "acc_capacity"] = (
-                np.maximum(
-                    road_links.loc[
-                        road_links["damage_level"] == "moderate", "designed_capacity"
-                    ]
-                    * cap_moderate,
-                    road_links.loc[
-                        road_links["damage_level"] == "moderate", "acc_capacity"
-                    ],
-                )
-            )
-            road_links.loc[
-                road_links["damage_level"] == "extensive", "acc_capacity"
-            ] = np.maximum(
-                road_links.loc[
-                    road_links["damage_level"] == "extensive", "designed_capacity"
-                ]
-                * cap_extensive,
-                road_links.loc[
-                    road_links["damage_level"] == "extensive", "acc_capacity"
-                ],
-            )
-            road_links.loc[road_links["damage_level"] == "severe", "acc_capacity"] = (
-                np.maximum(
-                    road_links.loc[
-                        road_links["damage_level"] == "severe", "designed_capacity"
-                    ]
-                    * cap_severe,
-                    road_links.loc[
-                        road_links["damage_level"] == "severe", "acc_capacity"
-                    ],
-                )
-            )
+        # drop the disrupted links
+        valid_road_links = road_links[
+            (road_links.acc_capacity > 0) & (road_links.acc_speed > 0)
+        ].reset_index(drop=True)
 
-            """edge speeds: initial_speeds"""
-            road_links["acc_speed"] = road_links["initial_flow_speeds"]
+        # create network
+        network = func.create_igraph_network(valid_road_links)
 
         # run rerouting analysis
-        network, road_links = func.create_igraph_network(road_links)
-        road_links, isolation, odpfc = func.network_flow_model(
-            road_links, network, disrupted_od, flow_breakpoint_dict
+        valid_road_links, isolation, odpfc = func.network_flow_model(
+            valid_road_links, network, disrupted_od, flow_breakpoint_dict
         )
+
+        # update key variables
+        road_links.set_index("e_id", inplace=True)
+        valid_road_links.set_index("e_id", inplace=True)
+        road_links[["acc_flow", "acc_capacity", "acc_speed"]].update(
+            valid_road_links[["acc_flow", "acc_capacity", "acc_speed"]]
+        )
+        road_links.reset_index(inplace=True)
 
         odpfc = pd.DataFrame(
             odpfc,
@@ -170,10 +252,22 @@ def main():
                 "destination_node",
                 "path",
                 "flow",
-                "operating_cost",
-                "time_cost",
-                "toll_cost",
+                "operating_cost_per_flow",
+                "time_cost_per_flow",
+                "toll_cost_per_flow",
             ],
+        )
+
+        odpfc.path = odpfc.path.apply(tuple)
+        odpfc = odpfc.groupby(
+            by=["origin_node", "destination_node", "path"], as_index=False
+        ).agg(
+            {
+                "flow": "sum",
+                "operating_cost_per_flow": "first",
+                "time_cost_per_flow": "first",
+                "toll_cost_per_flow": "first",
+            }
         )
 
         isolation = pd.DataFrame(
@@ -183,30 +277,34 @@ def main():
                 "destination_node",
                 "path",
                 "flow",
-                "operating_cost",
-                "time_cost",
-                "toll_cost",
+                "operating_cost_per_flow",
+                "time_cost_per_flow",
+                "toll_cost_per_flow",
             ],
         )
 
         # export rerouting results
-        road_links.to_parquet(
-            base_path.parent
-            / "outputs"
-            / "disruption_analysis"
-            / f"edge_flows_{day}.pq"
-        )
-        isolation.to_csv(
-            base_path.parent
-            / "outputs"
-            / "disruption_analysis"
-            / f"isolated_odf_{day}.csv",
-            index=False,
-        )
-        odpfc.to_csv(
-            base_path.parent / "outputs" / "disruption_analysis" / f"odpfc_{day}.csv",
-            index=False,
-        )
+        if day in [0, 15, 30, 60, 90, 110]:
+            road_links.to_parquet(
+                base_path.parent
+                / "outputs"
+                / "disruption_analysis"
+                / f"edge_flows_{day}.pq"
+            )
+            isolation.to_csv(
+                base_path.parent
+                / "outputs"
+                / "disruption_analysis"
+                / f"isolated_odf_{day}.csv",
+                index=False,
+            )
+            odpfc.to_csv(
+                base_path.parent
+                / "outputs"
+                / "disruption_analysis"
+                / f"odpfc_{day}.csv",
+                index=False,
+            )
 
 
 if __name__ == "__main__":
