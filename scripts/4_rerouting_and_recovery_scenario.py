bf052896d600a5bc386b7492ce7f1dfcb811109d
Yue Li <yue.li@ouce.ox.ac.uk>
2025-09-11 16:23:10 +0000

    Revise model to compute rerouting cost (instead of total cost after disruption), and to calculate daily flow changes

--- scripts/4_rerouting_and_recovery_scenario.py
+++ scripts/4_rerouting_and_recovery_scenario.py
@@ -30,38 +30,37 @@ def have_common_items(list1: List, list2: List) -> bool:
 def bridge_recovery(
     day: int,
     damage_level: str,
-    designed_capacity: float,
-    current_capacity: float,
+    pre_event_capacity: float,
+    acc_capacity: float,
     bridge_recovery_dict: Dict,
     event_day: bool,
 ) -> Tuple[float, float]:
     if event_day:
         if damage_level in ["extensive", "severe"]:
-            current_capacity = 0
+            acc_capacity = 0
     else:
         if damage_level != "no":
             recovery_rate = bridge_recovery_dict.get(damage_level, [])[day]
-            current_capacity = max(designed_capacity * recovery_rate, current_capacity)
-    return current_capacity
+            acc_capacity = max(pre_event_capacity * recovery_rate, acc_capacity)
+    return acc_capacity
 
 
 def ordinary_road_recovery(
     day: int,
     damage_level: str,
-    designed_capacity: float,
-    current_capacity: float,
+    pre_event_capacity: float,
+    acc_capacity: float,
     road_recovery_dict: Dict,
     event_day: bool,
 ) -> Tuple[float, float]:
     if event_day:  # the occurance of damage
         if damage_level in ["extensive", "severe"]:
-            current_capacity = 0
+            acc_capacity = 0
     else:
         if damage_level != "no":
             recovery_rate = road_recovery_dict.get(damage_level, [])[day]
-            current_capacity = max(designed_capacity * recovery_rate, current_capacity)
-
-    return current_capacity
+            acc_capacity = max(pre_event_capacity * recovery_rate, acc_capacity)
+    return acc_capacity
 
 
 def load_scenarios(base_path: Path) -> Tuple[Dict, Dict]:
@@ -113,14 +112,14 @@ def main(
         base_path.parent
         / "results"
         / "disruption_analysis"
+        / "revision"
         / str(depth_thres)
         / "links"
-        / f"road_links_{flood_key}.gpq"  # Thames Lloyd's RDS
+        / f"road_links_{flood_key}.gpq"
     )
     road_links["breakpoint_flows"] = road_links["combined_label"].map(
         flow_breakpoint_dict
     )
-    road_links["designed_capacity"] = road_links["current_capacity"]
 
     # Load OD path file
     od_path_file = pd.read_parquet(
@@ -131,7 +130,7 @@ def main(
     # Identify disrupted links
     disrupted_links = (
         road_links.loc[
-            (road_links.max_speed < road_links.current_speed)
+            (road_links.max_speed < road_links.current_speed)  # all damage levels
             | (road_links.damage_level_max == "extensive")
             | (road_links.damage_level_max == "severe"),
             "e_id",
@@ -147,19 +146,18 @@ def main(
 
     # Recovery analysis loop
     cDict = {}
-    # revise this to batch-process...
-    # for scenario_idx in range(len(scenarios)):  # 13 scenarios
-    print(f"Rerouting Analysis on Scenario-{scenario_idx}...")
+    logging.info(f"Rerouting Analysis on Scenario-{scenario_idx}...")
     event_day = conditions[scenario_idx] == 1
     # update edge capacities
-    print("Updating edge capacities...")
-    road_links["current_capacity"] = road_links.progress_apply(
+    logging.info("Updating edge capacities...")
+    road_links["acc_capacity"] = road_links["current_capacity"]
+    road_links["acc_capacity"] = road_links.progress_apply(
         lambda row: (
             bridge_recovery(
                 scenario_idx,
                 row["damage_level_max"],
-                row["designed_capacity"],
                 row["current_capacity"],
+                row["acc_capacity"],
                 bridge_recovery_dict,
                 event_day,
             )
@@ -168,8 +166,8 @@ def main(
                 ordinary_road_recovery(
                     scenario_idx,
                     row["damage_level_max"],
-                    row["designed_capacity"],
                     row["current_capacity"],
+                    row["acc_capacity"],
                     road_recovery_dict,
                     event_day,
                 )
@@ -191,32 +189,35 @@ def main(
     disrupted_od = od_path_file.loc[
         od_path_file["min_capacities_of_disrupted_links"].notnull()
     ]
-    disrupted_od["flow"] = np.maximum(
+    # some flows could be still send to the original path
+    # with the rest flows disrupted
+    disrupted_od["disrupted_flow"] = np.maximum(
         0,
         disrupted_od["flow"] - disrupted_od["min_capacities_of_disrupted_links"],
-    )
+    )  # disrupted_od_flows
 
-    # Adjust road flows
+    # Restore capacity for non-disrupted roads
+    disrupted_edge_flow = get_flow_on_edges(
+        disrupted_od, "e_id", "path", "disrupted_flow"
+    )  # redistribute those od back to road links
+
+    # disrupted_edge_flow.rename(columns={"flow": "disrupted_flow"}, inplace=True)
     road_links["disrupted_flow"] = 0.0
-    disrupted_edge_flow = get_flow_on_edges(disrupted_od, "e_id", "path", "flow")
-    disrupted_edge_flow.rename(columns={"flow": "disrupted_flow"}, inplace=True)
-
     road_links = road_links.set_index("e_id")
     road_links.update(disrupted_edge_flow.set_index("e_id")["disrupted_flow"])
     road_links = road_links.reset_index()
 
-    road_links["acc_capacity"] = road_links["current_capacity"] + np.where(
-        road_links["damage_level_max"].isin(["extensive", "severe"]),
-        0,
-        road_links["disrupted_flow"],
+    # initial key variables
+    logging.info("Initialising key variables for flow modelling...")
+    road_links["acc_capacity"] = (
+        road_links["acc_capacity"] + road_links["disrupted_flow"]
     )
-
-    # Update edge speeds
-    print("Updating edge speeds...")
-    road_links["acc_speed"] = road_links.apply(
+    road_links["acc_flow"] = road_links["current_flow"] - road_links["disrupted_flow"]
+    logging.info("Updating road speed limits...")
+    road_links["acc_speed"] = road_links.progress_apply(
         lambda x: func.update_edge_speed(
             x["combined_label"],  # constant
-            x["current_flow"],  # variable
+            x["acc_flow"],  # variable
             x["initial_flow_speeds"],  # constant
             x["min_flow_speeds"],  # constant
             x["breakpoint_flows"],  # constant
@@ -224,34 +225,53 @@ def main(
         axis=1,
     )
     if event_day:  # the occurance of damage
-        road_links["acc_speed"] = road_links[["current_speed", "max_speed"]].min(axis=1)
+        road_links["acc_speed"] = road_links[["acc_speed", "max_speed"]].min(axis=1)
 
-    # initial key variables
-    road_links["acc_flow"] = 0
+    logging.info("Preparing disrupted OD matrix...")
     disrupted_od.rename(columns={"flow": "Car21"}, inplace=True)
-    # disrupted_od = disrupted_od.head(100)  #!!! debug
+
+    total_cost = (
+        disrupted_od.disrupted_flow
+        * (
+            disrupted_od.operating_cost_per_flow
+            + disrupted_od.time_cost_per_flow
+            + disrupted_od.toll_cost_per_flow
+        )
+    ).sum()
+    logging.info(f"The original travel costs for disrupted od: £{total_cost}")
 
     # create network (time-consuming when updating network edge index)
-    print("Creating igraph network...")
+    logging.info("Creating igraph network...")
     valid_road_links = road_links[
         (road_links["acc_capacity"] > 0) & (road_links["acc_speed"] > 0)
     ].reset_index(drop=True)
     network, valid_road_links = func.create_igraph_network(valid_road_links)
 
     # run flow rerouting analysis
-    print("Running flow simulation...")
-    valid_road_links, isolation, _, cList = func.network_flow_model(
+    logging.info("Running flow simulation...")
+    valid_road_links, isolation, _, (_, _, _, total_cost2) = func.network_flow_model(
         valid_road_links,
         network,
         disrupted_od[["origin_node", "destination_node", "Car21"]],
         flow_breakpoint_dict,
         num_of_cpu=number_of_cpu,
     )
-    cDict[scenario_idx] = cList
+    rerouting_cost = total_cost2 - total_cost
+    logging.info(f"The total travel costs after disruption: £{total_cost2}")
+    logging.info(f"The rerouting cost for scenario {scenario_idx}: £{rerouting_cost}")
+
+    # rerouting costs
+    cDict[scenario_idx] = rerouting_cost
+    cost_df = pd.DataFrame.from_dict(
+        cDict, orient="index", columns=["rerouting_cost"]
+    ).reset_index()
+    cost_df.rename(columns={"index": "scenario"}, inplace=True)
+    # edge flows
     road_links = road_links.set_index("e_id")
     road_links.update(valid_road_links.set_index("e_id")["acc_flow"])
     road_links = road_links.reset_index()
-
+    road_links["change_flow"] = road_links["acc_flow"] - road_links["current_flow"]
+    # trip isolations
     isolation_df = pd.DataFrame(
         isolation,
         columns=[
@@ -261,38 +281,29 @@ def main(
         ],
     )
 
+    logging.info("Saving outputs to disk...")
     out_path = (
         base_path.parent
         / "results"
         / "rerouting_analysis"
-        / "scenarios"
+        / "revision"
         / str(depth_thres)  # e.g.,30
         / str(flood_key)  # e.g.,17
     )
     out_path.mkdir(parents=True, exist_ok=True)
-
-    print("Saving results to disk...")
+    cost_df.to_csv(out_path / f"cost_matrix_{scenario_idx}.csv", index=False)
     isolation_df.to_csv(
         out_path / f"trip_isolations_{scenario_idx}.csv",
         index=False,
     )
     road_links.to_parquet(out_path / f"edge_flows_{scenario_idx}.gpq")
 
-    cost_df = pd.DataFrame.from_dict(
-        cDict, orient="index", columns=["time", "fuel", "toll", "total"]
-    ).reset_index()
-    cost_df.rename(columns={"index": "scenario"}, inplace=True)
-    cost_df.to_csv(out_path / f"cost_matrix_{scenario_idx}.csv", index=False)
-
-    del isolation
-    del valid_road_links
-
 
 if __name__ == "__main__":
     try:
         depth_thres = int(sys.argv[1])
     except (IndexError, ValueError):
-        print(
+        logging.info(
             "Error: Please provide the flood depth for road closure "
             "(e.g., 15, 30 or 60 cm) as the first argument!"
         )
@@ -301,19 +312,21 @@ if __name__ == "__main__":
     try:
         number_of_cpu = int(sys.argv[2])
     except (IndexError, ValueError):
-        print("Error: Please provide the number of CPUs as the second argument!")
+        logging.info("Error: Please provide the number of CPUs as the second argument!")
         sys.exit(1)
 
     try:
         flood_key = int(sys.argv[3])
     except (IndexError, ValueError):
-        print("Error: Please provide the flood key!")
+        logging.info("Error: Please provide the flood key!")
         sys.exit(1)
 
     try:
         scenario_idx = int(sys.argv[4])  # 0-12
     except (IndexError, ValueError):
-        print("Error: Please provide the scenario index (0-12) as the fourth argument!")
+        logging.info(
+            "Error: Please provide the scenario index (0-12) as the fourth argument!"
+        )
         sys.exit(1)
 
     logging.basicConfig(
