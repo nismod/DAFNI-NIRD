3d9b315d60f15ddd4d3ebe1ff1582ae9a1710a7a
Yue Li <yue.li@ouce.ox.ac.uk>
2026-03-17 16:24:13 +0000

    estimates future population distribution at OA level in England

--- scripts/nist-road/pop_oa_estimates.py
+++ scripts/nist-road/pop_oa_estimates.py
@@ -6,30 +6,19 @@ from nird.utils import load_config
 
 dafni_path = Path(load_config()["paths"]["base_path"])
 nist_path = Path(load_config()["paths"]["nist_path"])
+nist_path = nist_path / "incoming" / "20260216 - inputs to OxfUni models"
 
 # %%
 cluster = gpd.read_parquet(
-    nist_path
-    / "incoming"
-    / "20260216 - inputs to OxfUni models"
-    / "20260216 - inputs to OxfUni models"
-    / "cluster.parquet"
+    nist_path / "20260216 - inputs to OxfUni models" / "cluster.parquet"
 )
 
 cluster_pop_ppp = pd.read_csv(
-    nist_path
-    / "incoming"
-    / "20260216 - inputs to OxfUni models"
-    / "20260216 - inputs to OxfUni models"
-    / "cluster_pop_ppp_forecasts.csv"
+    nist_path / "20260216 - inputs to OxfUni models" / "cluster_pop_ppp_forecasts.csv"
 )
 
 cluster_pop_hhh = pd.read_csv(
-    nist_path
-    / "incoming"
-    / "20260216 - inputs to OxfUni models"
-    / "20260216 - inputs to OxfUni models"
-    / "cluster_pop_hhh_forecasts.csv"
+    nist_path / "20260216 - inputs to OxfUni models" / "cluster_pop_hhh_forecasts.csv"
 )
 
 oa21 = gpd.read_parquet(
@@ -58,31 +47,39 @@ grouped["ratio"] = grouped["OA_POP_2021"].apply(
 joined = grouped.explode(["OA21CD", "OA_POP_2021", "ratio"], ignore_index=True)
 
 # %%
+cluster_pop_ppp["cluster_ppp_2025"] = cluster_pop_ppp["2025"]
 cluster_pop_ppp["cluster_ppp_2030"] = cluster_pop_ppp["2030"]
 cluster_pop_ppp["cluster_ppp_2050"] = cluster_pop_ppp["2050"]
 joined_ppp = joined.merge(
-    cluster_pop_ppp[["cluster_id", "cluster_ppp_2030", "cluster_ppp_2050"]],
+    cluster_pop_ppp[
+        ["cluster_id", "cluster_ppp_2025", "cluster_ppp_2030", "cluster_ppp_2050"]
+    ],
     on="cluster_id",
     how="left",
 )
+joined_ppp["OA21_PPP_2025"] = joined_ppp["ratio"] * joined_ppp["cluster_ppp_2025"]
 joined_ppp["OA21_PPP_2030"] = joined_ppp["ratio"] * joined_ppp["cluster_ppp_2030"]
 joined_ppp["OA21_PPP_2050"] = joined_ppp["ratio"] * joined_ppp["cluster_ppp_2050"]
 
 # %%
+cluster_pop_hhh["cluster_hhh_2025"] = cluster_pop_hhh["2025"]
 cluster_pop_hhh["cluster_hhh_2030"] = cluster_pop_hhh["2030"]
 cluster_pop_hhh["cluster_hhh_2050"] = cluster_pop_hhh["2050"]
 joined_hhh = joined.merge(
-    cluster_pop_hhh[["cluster_id", "cluster_hhh_2030", "cluster_hhh_2050"]],
+    cluster_pop_hhh[
+        ["cluster_id", "cluster_hhh_2025", "cluster_hhh_2030", "cluster_hhh_2050"]
+    ],
     on="cluster_id",
     how="left",
 )
+joined_hhh["OA21_HHH_2025"] = joined_hhh["ratio"] * joined_hhh["cluster_hhh_2025"]
 joined_hhh["OA21_HHH_2030"] = joined_hhh["ratio"] * joined_hhh["cluster_hhh_2030"]
 joined_hhh["OA21_HHH_2050"] = joined_hhh["ratio"] * joined_hhh["cluster_hhh_2050"]
 
 # %%
 grouped_ppp = (
     joined_ppp.groupby(by=["OA21CD"], as_index=False)[
-        ["OA21_PPP_2030", "OA21_PPP_2050"]
+        ["OA21_PPP_2025", "OA21_PPP_2030", "OA21_PPP_2050"]
     ]
     .sum()
     .merge(oa21[["OA21CD", "OA_POP_2021"]], on="OA21CD", how="left")
@@ -90,7 +87,7 @@ grouped_ppp = (
 
 grouped_hhh = (
     joined_hhh.groupby(by=["OA21CD"], as_index=False)[
-        ["OA21_HHH_2030", "OA21_HHH_2050"]
+        ["OA21_HHH_2025", "OA21_HHH_2030", "OA21_HHH_2050"]
     ]
     .sum()
     .merge(oa21[["OA21CD", "OA_POP_2021"]], on="OA21CD", how="left")
