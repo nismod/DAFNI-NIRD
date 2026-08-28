0e79e084cbc9e97b6465276df84fd74863c95ada
Yue Li <yue.li@ouce.ox.ac.uk>
2026-08-28 17:19:31 +0100

    restore the previous revision : disrupted intersection length should not be aggregated to links

--- scripts/macchub/2_intersection_analysis_NI.py
+++ scripts/macchub/2_intersection_analysis_NI.py
@@ -391,7 +391,7 @@ def features_with_damage(
 
     intersections_gp = intersections.groupby("e_id", as_index=False).agg(
         {
-            "length": "sum",
+            # "length": "sum",
             "flood_depth_max": "max",
             "damage_level_max": "max",
         }
@@ -405,7 +405,7 @@ def features_with_damage(
         how="left",
         on="e_id",
     )
-    features["length"] = features["length"].fillna(0.0)
+    # features["length"] = features["length"].fillna(0.0)
     features["flood_depth_max"] = features["flood_depth_max"].fillna(0.0)
     features["damage_level_max"] = features["damage_level_max"].fillna("no")
 
